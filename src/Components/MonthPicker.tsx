849396240ab80feeea41480061b9e2e4a52af5f1
Hossein Davoodi <H.Davoodi@trenkwalder.com>
2024-03-21 10:12:35 +0300

    Add logic of customized month picker

--- src/Components/MonthPicker.tsx
+++ src/Components/MonthPicker.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 interface MonthPickerProps {
   onMonthChange: (selectedMonth: number) => void;
@@ -15,39 +15,91 @@ const MonthPicker: React.FC<MonthPickerProps> = ({
   const [selectedYear, setSelectedYear] = useState<number>(
     new Date().getFullYear()
   );
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const modalRef = useRef<HTMLDivElement>(null);
 
-  const handleMonthChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const month = Number(e.target.value);
+  const handleMonthChange = (month: number) => {
     setSelectedMonth(month);
     onMonthChange(month);
+    setShowModal(false);
   };
 
-  const handleYearChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const year = Number(e.target.value);
+  const handleYearChange = (year: number) => {
     setSelectedYear(year);
     onYearChange(year);
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      setShowModal(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const months = Array.from({ length: 12 }, (_, i) => i + 1);
+  const currentDate = new Date(selectedYear, selectedMonth - 1);
+  const formattedDate = currentDate.toLocaleDateString(undefined, {
+    month: "short",
+    year: "numeric",
+  });
+
   return (
     <div className="month-picker">
-      <select value={selectedMonth} onChange={handleMonthChange}>
-        {Array.from({ length: 12 }, (_, i) => i + 1).map((month) => (
-          <option key={month} value={month}>
-            {new Date(selectedYear, month - 1).toLocaleString("default", {
-              month: "long",
-            })}
-          </option>
-        ))}
-      </select>
-      <select value={selectedYear} onChange={handleYearChange}>
-        {Array.from({ length: 5 }, (_, i) => new Date().getFullYear() - i).map(
-          (year) => (
-            <option key={year} value={year}>
-              {year}
-            </option>
-          )
-        )}
-      </select>
+      {!showModal && (
+        <div
+          className="date-picker-icon"
+          onClick={() => setShowModal(!showModal)}
+        >
+          Month Picker
+        </div>
+      )}
+      {showModal && (
+        <div ref={modalRef} className="month-picker-modal">
+          <div className="date-picker-header">{formattedDate}</div>
+          <div className="year-selector">
+            <button
+              className="arrowBtn"
+              onClick={() => handleYearChange(selectedYear - 1)}
+            >
+              &lt;
+            </button>
+            <span>{selectedYear}</span>
+            <button
+              className="arrowBtn"
+              onClick={() =>
+                handleYearChange(
+                  selectedYear === new Date().getFullYear()
+                    ? selectedYear
+                    : selectedYear + 1
+                )
+              }
+            >
+              &gt;
+            </button>
+          </div>
+          <div className="month-grid">
+            {months.map((month) => (
+              <div
+                key={month}
+                className={`month-option ${
+                  month === selectedMonth ? "selected" : ""
+                }`}
+                onClick={() => handleMonthChange(month)}
+              >
+                {new Date(selectedYear, month - 1).toLocaleString("default", {
+                  month: "short",
+                })}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
